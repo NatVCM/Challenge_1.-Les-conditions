1549772fc7bf71dfab741bec91f117667c9edc12
NatVCM <147696519+NatVCM@users.noreply.github.com>
2024-04-09 17:17:22 +0200

    add de challenge

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Challenge_1._Les_conditions;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Type a minimum integer number: ");
+        string saisiUtilisateurminimum = Console.ReadLine();
+        Console.Write("Type a maximum integer number: ");
+        string saisiUtilisateurmaximum = Console.ReadLine();
+
+        int nombreminimum = Convert.ToInt32(saisiUtilisateurminimum);
+        int nombremaximum = Convert.ToInt32(saisiUtilisateurmaximum);
+
+        Console.Write("Type an integer number between 0 and 100: ");
+        string saisiUtilisateur = Console.ReadLine();
+        int nombre = Convert.ToInt32(saisiUtilisateur);
+
+        if (nombre >= 0 && nombre <= 100)
+        {
+            Console.WriteLine("Resulting integer:" + nombre);
+                }
+        else if (nombre > 100)
+        {
+            Console.WriteLine("You have entered " + nombre + " which is greater than 100 which is the maximum");
+        }
+        else 
+        {
+            Console.WriteLine("You have entered " + nombre +" which is lower than 0 which is the minimum");
+        }
+
+
+
+    }
+}
